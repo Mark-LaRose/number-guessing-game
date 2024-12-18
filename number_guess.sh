43b503f06fbfc83068c1354d6cc4f49ef55b81d9
Mark LaRose <PlanZero.2on@gmail.com>
2024-12-18 04:26:00 +0000

    feat: check if user is new or returning

--- number_guess.sh
+++ number_guess.sh
@@ -3,3 +3,17 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo "Enter your username:"
 read USERNAME
+
+# Check if username exists in the database
+USER_DATA=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$USERNAME'")
+
+if [[ -z $USER_DATA ]]; then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  # Insert the new user into the database
+  INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+else
+  echo "$USER_DATA" | while IFS="|" read GAMES_PLAYED BEST_GAME
+  do
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  done
+fi
